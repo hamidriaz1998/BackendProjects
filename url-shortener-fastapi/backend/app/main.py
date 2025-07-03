c2f07a885bcdbd8b054e89c10aa6234fc9050013
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2025-07-03 11:59:59 +0500

    Reorganize directory structure

--- url-shortener-fastapi/backend/app/main.py
+++ url-shortener-fastapi/backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth_router
 from app.api import url_router
@@ -6,6 +6,12 @@ from app.api import url_router
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(url_router)
+
+@app.get("/test")
+def test(request: Request):
+    user_agent = request.headers.get("User-Agent")
+    referrer = request.headers.get("Referer")
+    return {"ip": request.client.host, "user_agent": user_agent, "referrer": referrer} # pyright: ignore
 
 # Configure Cors
 app.add_middleware(
